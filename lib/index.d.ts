25c2a7f09de5520ce395dd703094c221e2799041
chieveit <javachieveit@163.com>
2020-11-19 09:03:17 +0000

    完成简单的搜索功能

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,12 @@
-export {};
+export interface Doc {
+    id: any;
+    [key: string]: any;
+}
+export default function engine(): {
+    index(indexFields: string[], doc: Doc): void;
+    search(text: string): Doc[];
+    dict(path: string): Promise<void>;
+    tokens(): Iterable<import("./trie").Token>;
+    cut: (text: string, exceptSelf?: boolean) => import("./trie").Token[];
+    fullCut: (text: string) => import("./trie").Token[];
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,52 @@
-import trie from './cutter';
+import trie from './trie';
+
+export interface Doc {
+  id: any;
+  [key: string]: any;
+}
+
+export default function engine() {
+  const index = trie();
+  const docs: {
+    [id: string]: {
+      id: string;
+      [key: string]: any;
+    };
+  } = {};
+
+  return {
+    ...index,
+    index(indexFields: string[], doc: Doc) {
+      docs[doc.id] = doc;
+
+      let fields = indexFields.map((v) => doc[v]);
+      index.index(fields.join(' '), doc.id);
+    },
+    search(text: string): Doc[] {
+      const tokens = index.cut(text);
+      if (tokens.length == 0) {
+        return [];
+      }
+
+      const [first, ...rest] = tokens;
+
+      let intersect: {
+        [id: string]: number;
+      } = first.docs;
+      for (let t of rest) {
+        for (let id in intersect) {
+          if (!t.docs[id]) {
+            delete intersect[id];
+          }
+        }
+      }
+
+      const result: Doc[] = [];
+      for (let id in intersect) {
+        result.push(docs[id]);
+      }
+
+      return result;
+    },
+  };
+}
